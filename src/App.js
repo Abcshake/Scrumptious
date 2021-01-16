bbd6fa27dbd05fe2c8b62d9503ea1d6ba6e86e29
Avishek Chakrabarty <avishiek@gmail.com>
2021-01-15 22:42:27 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+ 
 
-function App() {
+const business = {
+  imageSrc: 'https://images.pexels.com/photos/2092906/pexels-photo-2092906.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  name: 'Nicolo Pastaria',
+  address: '1030 Bohemian Drive',
+  city: 'Lucia',
+  state: 'ON',
+  zipCode: 'R5Y3E4',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+ }
+
+ const businesses = [
+   business,
+   business,
+   business,
+   business,
+   business,
+   business
+ ];
+
+
+class App extends React.Component {
+
+  searchYelp(term,location,sortBy){
+    console.log(`We found ${term} at ${location} in the follow ${sortBy}`);
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Scrumptious</h1>
+      <SearchBar searchYelp={this.searchYelp} />
+      <BusinessList businesses={businesses} />
     </div>
   );
+  }
 }
 
 export default App;
