7ae0b54e38f0ee1584e90f8ac2547785e71d63de
Avi <avishiek@gmail.com>
2023-07-21 21:39:48 -0400

    tried to fix cors error

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,7 +2,7 @@ const apiKey = process.env.REACT_APP_apikey;
 const Yelp = {
 
 search(term,location,sortBy) {
-    return fetch(`https://corsanywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
         {
         headers:     
          {
